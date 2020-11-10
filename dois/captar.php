454eb10955cb70b78038e7a7ce668f516f3af2f3
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-10 17:02:42 +0000

    Update captar.php, captar.php, and captar.php

--- dois/captar.php
+++ dois/captar.php
@@ -1,6 +1,8 @@
 <?php
-	$nome = $_GET["txt_nome"];
-	$idade = $_GET["txt_idade"];
-	echo "Nome: " .$nome. "<br>";
-	echo "Idade: " .$idade;
+	if (isset($_GET["txt_nome"])&&isset($_GET["txt_idade"])) {
+		$nome = $_GET["txt_nome"];
+		$idade = $_GET["txt_idade"];
+		echo "Nome: " .$nome. "<br>";
+		echo "Idade: " .$idade;
+	}
 ?>
--- um/captar.php
+++ um/captar.php
@@ -1,6 +1,9 @@
 <?php
-	$nome = $_POST["txt_nome"];
-	$idade = $_POST["txt_idade"];
-	echo "Nome: " .$nome. "<br>";
-	echo "Idade: " .$idade;
+	if (isset($_POST["txt_nome"])&&isset($_POST["txt_idade"])) {
+		
+		$nome = $_POST["txt_nome"];
+		$idade = $_POST["txt_idade"];
+		echo "Nome: " .$nome. "<br>";
+		echo "Idade: " .$idade;
+	}
 ?>
